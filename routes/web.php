9dafdd85225d599da1f5f4d607867874fbd76157
kkuldip <kuldip.patgiri@yahoo.com>
2023-12-22 11:49:36 +0530

    Markerter

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\User\Auth\LoginController;
+use App\Http\Controllers\User\Auth\RegisterController;
+use App\Http\Controllers\User\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[HomeController::class,'index'])->name('user.home');
+Route::get('/login',[LoginController::class,'index'])->name('user.login');
+Route::get('/register',[RegisterController::class,'index'])->name('user.register');
